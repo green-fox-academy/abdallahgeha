72743a6c59562c210742b9d545af3159830a369e
abdallahgeha <geha.abdallah@gmail.com>
2019-09-12 23:56:07 +0200

    solved diamonds for even and odd line number

--- week-01/day-4/diamond.ts
+++ week-01/day-4/diamond.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let lineCount: number = 7;
+let lineCount: number = 8;
 
 // Write a program that draws a
 // diamond like this:
@@ -15,31 +15,58 @@ let lineCount: number = 7;
 //
 // The diamond should have as many lines as lineCount is
 
-for ( let i : number = 0 ; i < lineCount ; i++) {
-    let a: string = '' ;
 
-    if ( i <= (lineCount/2)) {
-        for ( let j: number = 1 ; j <= lineCount ; j++){
-            if (j < (((lineCount+1)/2)-i) || j > ((((lineCount+1)/2)+i))){
-                a = a + ' ' ;
-            } else {
-                a = a + '*' ;
+if( lineCount%2 == 0){
+    for ( let i : number = 0 ; i < lineCount ; i++) {
+        let a: string = '' ;
+
+        if ( i <= (lineCount/2)) {
+            for ( let j: number = 1 ; j <= lineCount ; j++){
+                if (j < (((lineCount)/2)-i) || j > ((((lineCount)/2)+i))){
+                    a = a + ' ' ;
+                } else {
+                    a = a + '*' ;
+                }
+            }
+        } else {
+            for ( let j: number = 1 ; j <= lineCount ; j++){
+                if ((j < (((lineCount)/2))-((lineCount-i-1))) || (j > ((lineCount)/2) + ((lineCount-i-1)))  ){
+                    a = a + ' ' ;
+                } else {
+                    a = a + '*' ;
+                }
             }
         }
-    } else {
-        for ( let j: number = 1 ; j <= lineCount ; j++){
-            if ((j < (((lineCount+1)/2))-((lineCount-i-1))) || (j > ((lineCount+1)/2) + ((lineCount-i-1)))  ){
-                a = a + ' ' ;
-            } else {
-                a = a + '*' ;
+
+        console.log(a);
+    }
+} else {
+    for ( let i : number = 0 ; i < lineCount ; i++) {
+        let a: string = '' ;
+
+        if ( i <= (lineCount/2)) {
+            for ( let j: number = 1 ; j <= lineCount ; j++){
+                if (j < (((lineCount+1)/2)-i) || j > ((((lineCount+1)/2)+i))){
+                    a = a + ' ' ;
+                } else {
+                    a = a + '*' ;
+                }
+            }
+        } else {
+            for ( let j: number = 1 ; j <= lineCount ; j++){
+                if ((j < (((lineCount+1)/2))-((lineCount-i-1))) || (j > ((lineCount+1)/2) + ((lineCount-i-1)))  ){
+                    a = a + ' ' ;
+                } else {
+                    a = a + '*' ;
+                }
             }
         }
+
+
+
+        console.log(a);
     }
 
-
-
-    console.log(a);
 }
-
 
 
