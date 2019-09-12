cd88d5df0709dad4c0f36611fca6d0ad69d70e58
abdallahgeha <geha.abdallah@gmail.com>
2019-09-12 23:48:33 +0200

    finally solved the diamond

--- week-01/day-4/diamond.ts
+++ week-01/day-4/diamond.ts
@@ -15,12 +15,29 @@ let lineCount: number = 7;
 //
 // The diamond should have as many lines as lineCount is
 
-for ( let i : number = 1 ; i <= lineCount ; i++) {
+for ( let i : number = 0 ; i < lineCount ; i++) {
     let a: string = '' ;
 
+    if ( i <= (lineCount/2)) {
+        for ( let j: number = 1 ; j <= lineCount ; j++){
+            if (j < (((lineCount+1)/2)-i) || j > ((((lineCount+1)/2)+i))){
+                a = a + ' ' ;
+            } else {
+                a = a + '*' ;
+            }
+        }
+    } else {
+        for ( let j: number = 1 ; j <= lineCount ; j++){
+            if ((j < (((lineCount+1)/2))-((lineCount-i-1))) || (j > ((lineCount+1)/2) + ((lineCount-i-1)))  ){
+                a = a + ' ' ;
+            } else {
+                a = a + '*' ;
+            }
+        }
+    }
 
 
-    
+
     console.log(a);
 }
 
