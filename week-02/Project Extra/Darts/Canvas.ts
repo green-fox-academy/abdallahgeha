dac8b7bc1aa6a73a2987b4f71d9d417e85173dd6
abdallahgeha <geha.abdallah@gmail.com>
2019-09-20 10:46:04 +0200

    add silver border

--- week-02/Project Extra/Darts/Canvas.ts
+++ week-02/Project Extra/Darts/Canvas.ts
@@ -7,6 +7,8 @@ const ctx = canvas.getContext('2d');
 let rMax : number = 320;
 ctx.translate(canvas.width/2,canvas.height/2);
 ctx.rotate(-Math.PI/20);
+ctx.strokeStyle = 'rgb(192,192,192)'
+ctx.lineWidth = 2;
 
 ctx.fillStyle = 'black';
 ctx.beginPath();
@@ -26,11 +28,13 @@ ctx.fillStyle = 'green';
 ctx.beginPath();
 ctx.arc(0,0,rMax/10,0,2*Math.PI);
 ctx.fill();
+ctx.stroke();
 
 ctx.fillStyle = 'red';
 ctx.beginPath();
 ctx.arc(0,0,rMax/20,0,2*Math.PI);
 ctx.fill();
+ctx.stroke();
 
 
 
@@ -55,6 +59,7 @@ function peice (peiceN : number ,w : number , r : number){
         ctx.arc(0,0,(instR),0,Math.PI/10);
         ctx.lineTo(0,0);
         ctx.fill();
+        ctx.stroke();
 
     }
 
