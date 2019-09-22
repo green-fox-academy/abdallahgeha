03f1b6a8077744d1244cf72ef845b02f51e8b4ef
abdallahgeha <geha.abdallah@gmail.com>
2019-09-22 23:49:02 +0200

    finished seperat piece drawings function

--- week-03/Demo project/13- Checkerboard/Canvas.ts
+++ week-03/Demo project/13- Checkerboard/Canvas.ts
@@ -27,6 +27,36 @@ function setPieceColor(factionColor:string){
     }
     
 }
+
+function drawKing (xPosition: number, yPosition: number, factionColor: string){
+    setPieceColor(factionColor);
+    ctx.save();
+    ctx.translate(xPosition,yPosition);
+
+    ctx.beginPath();
+    ctx.arc(squareSide*4/5,squareSide/2,squareSide/6,0,2*Math.PI);
+    ctx.arc(squareSide*1/5,squareSide/2,squareSide/6,0,2*Math.PI);
+    ctx.arc(squareSide/2,squareSide*2/5,squareSide/4,0,2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.lineWidth = 1.5;
+    ctx.fillRect(squareSide/4,squareSide*3/5,squareSide/2,squareSide/5);
+    ctx.strokeRect(squareSide/4,squareSide*3/5,squareSide/2,squareSide/5);
+
+    if(factionColor == 'black'){
+        ctx.fillStyle = 'white';
+    }else{
+        ctx.fillStyle = 'black';
+    }
+    ctx.fillRect((squareSide/2)-(squareSide/10), (squareSide/2)-(squareSide/20),squareSide*2/10, squareSide*1.5/20 );
+    ctx.fillRect((squareSide/2)-(squareSide/30),(squareSide/2)-(squareSide/10),squareSide*1.5/20, squareSide*2/10 );
+
+    ctx.restore();
+
+
+}
+
 function drawQueen(xPosition: number, yPosition: number, factionColor: string){
     setPieceColor(factionColor);
     ctx.save();
@@ -38,12 +68,12 @@ function drawQueen(xPosition: number, yPosition: number, factionColor: string){
     ctx.lineTo(squareSide*1.3/4,squareSide*2.6/5);
     ctx.lineTo(squareSide*1.3/4,squareSide*1/5);
     ctx.lineTo(squareSide*2/4,squareSide*2.6/5);
-    ctx.lineTo(squareSide*2/4,squareSide*2.6/5);
-
-
+    ctx.lineTo(squareSide*2.7/4,squareSide*1/5);
+    ctx.lineTo(squareSide*2.7/4,squareSide*2.6/5);
+    ctx.lineTo(squareSide*4/5,squareSide*2/5);
+    ctx.lineTo(squareSide*3/4,squareSide*3/5);
     ctx.stroke();
     ctx.fill();
-
 
     ctx.lineWidth = 1.5;
     ctx.fillRect(squareSide/4,squareSide*3/5,squareSide/2,squareSide/5);
@@ -161,8 +191,188 @@ function drawPawn(xPosition: number, yPosition: number, factionColor: string) {
     ctx.restore();
 
 }
-drawPawn(0,squareSide,'black');
-drawKnight(squareSide,0,'black');
-drawRook(0,0,'white');
-drawBishop(2*squareSide,0,'white');
-drawQueen(3*squareSide,0,'white');
+function drawNothing(xPosition: number, yPosition: number){
+    return;
+}
+
+//drawPawn(0,squareSide,'black');
+//drawKnight(squareSide,0,'black');
+//drawRook(0,0,'white');
+//drawBishop(2*squareSide,0,'white');
+//drawQueen(3*squareSide,0,'white');
+//drawNothing(0,0);
+//drawKing(4*squareSide,0,'black');
+
+let allThePieces = {
+    kingWhite : {
+        'xPosition' : 4,
+        'yPosition' : 7,
+        'color'     : 'white'
+    },
+    kingBlack : {
+        'xPosition' : 4,
+        'yPosition' : 0,
+        'color'     : 'black'
+    },
+    queenWhite : {
+        'xPosition' : 3,
+        'yPosition' : 7,
+        'color'     : 'white'
+    },
+    queenBlack : {
+        'xPosition' : 3,
+        'yPosition' : 7,
+        'color'     : 'black'
+    },
+    bishopWhite1 : {
+        'xPosition' : 2,
+        'yPosition' : 7,
+        'color'     : 'white'
+    },
+    bishopWhite2 :{
+        'xPosition' : 5,
+        'yPosition' : 7,
+        'color'     : 'white'
+    },
+    bishopBlack1 : {
+        'xPosition' : 2,
+        'yPosition' : 0,
+        'color'     : 'black'
+    },
+    bishopBlack2 : {
+        'xPosition' : 5,
+        'yPosition' : 0,
+        'color'     : 'black'
+    },
+    knightWhite1 : {
+        'xPosition' : 1,
+        'yPosition' : 7,
+        'color'     : 'white'
+    },
+    knighWhite2 : {
+        'xPosition' : 6,
+        'yPosition' : 7,
+        'color'     : 'black'
+    },
+    knighBlack1 : {
+        'xPosition' : 1,
+        'yPosition' : 0,
+        'color'     : 'black'
+    },
+    knighBlack2 : {
+        'xPosition' : 6,
+        'yPosition' : 7,
+        'color'     : 'white'
+    },
+    rookWhite1 : {
+        'xPosition' : 0,
+        'yPosition' : 7,
+        'color'     : 'white'
+    },
+    rookWhite2 : {
+        'xPosition' : 7,
+        'yPosition' : 7,
+        'color'     : 'white'
+    },
+    rookBlack1 : {
+        'xPosition' : 0,
+        'yPosition' : 0,
+        'color'     : 'black'
+    },
+    rookBlack2 : {
+        'xPosition' : 7,
+        'yPosition' : 0,
+        'color'     : 'black'
+    },
+    pawnWhite1 : {
+        'xPosition' : 0,
+        'yPosition' : 6,
+        'color'     : 'white'
+    },
+     pawnWhite2 : {
+        'xPosition' : 1,
+        'yPosition' : 6,
+        'color'     : 'white'
+    },
+     pawnWhite3 : {
+        'xPosition' : 2,
+        'yPosition' : 6,
+        'color'     : 'white'
+    },
+     pawnWhite4 : {
+        'xPosition' : 3,
+        'yPosition' : 6,
+        'color'     : 'white'
+    },
+     pawnWhite5 : {
+        'xPosition' : 4,
+        'yPosition' : 6,
+        'color'     : 'white'
+    },
+     pawnWhite6 : {
+        'xPosition' : 5,
+        'yPosition' : 6,
+        'color'     : 'white'
+    },
+     pawnWhite7 : {
+        'xPosition' : 6,
+        'yPosition' : 6,
+        'color'     : 'white'
+    },
+     pawnWhite8 : {
+        'xPosition' : 7,
+        'yPosition' : 6,
+        'color'     : 'white'
+    },
+     pawnblack1 : {
+        'xPosition' : 0,
+        'yPosition' : 1,
+        'color'     : 'black'
+    },
+     pawnblack2 : {
+        'xPosition' : 1,
+        'yPosition' : 1,
+        'color'     : 'black'
+    },
+     pawnblack3 : {
+        'xPosition' : 2,
+        'yPosition' : 1,
+        'color'     : 'black'
+    },
+     pawnblack4 : {
+        'xPosition' : 3,
+        'yPosition' : 1,
+        'color'     : 'black'
+    },
+     pawnblack5 : {
+        'xPosition' : 4,
+        'yPosition' : 1,
+        'color'     : 'black'
+    },
+     pawnblack6 : {
+        'xPosition' : 5,
+        'yPosition' : 1,
+        'color'     : 'black'
+    },
+     pawnblack7 : {
+        'xPosition' : 6,
+        'yPosition' : 1,
+        'color'     : 'black'
+    },
+     pawnblack8 : {
+        'xPosition' : 7,
+        'yPosition' : 1,
+        'color'     : 'black'
+    },
+};
+
+
+drawKing(allThePieces.kingWhite.xPosition*squareSide,allThePieces.kingWhite.yPosition*squareSide,allThePieces.kingWhite.color);
+
+
+
+
+
+
+
+
