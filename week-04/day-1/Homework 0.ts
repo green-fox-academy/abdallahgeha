e4a81910d62b7aad4177b91b95eaec6f9399e8b5
abdallahgeha <geha.abdallah@gmail.com>
2019-09-29 20:43:22 +0200

    mentor class added

--- week-04/day-1/Homework 0.ts
+++ week-04/day-1/Homework 0.ts
@@ -33,15 +33,19 @@ class Student {
     private _previousOrganization: string
     private _skippedDays: number
 
-    constructor(name?: string, age?: number, gender?: string) {
-        if (name && gender && age) {
+    constructor(name?: string, age?: number, gender?: string, previousOrganization?: string) {
+        if (name && gender && age && previousOrganization) {
             this._name = name
             this._age = age
             this._gender = gender
+            this._previousOrganization = previousOrganization
+            this._skippedDays = 0
         } else {
             this._name = 'jane Doe'
             this._age = 30
             this._gender = 'female'
+            this._previousOrganization = 'The School of Life'
+            this._skippedDays = 0
         }
     }
 
@@ -53,5 +57,37 @@ class Student {
         console.log("Be a junior software developer.")
     }
 
+    public skipDays(numberOfDays) {
+        this._skippedDays += numberOfDays
+    }
 
+}
+
+class Mentor {
+    private _name: string
+    private _age: number
+    private _gender: string
+    private _level: string
+
+    constructor(name?: string, age?: number, gender?: string, level?: string) {
+        if (name && gender && age) {
+            this._name = name
+            this._age = age
+            this._gender = gender
+            this._level = level
+        } else {
+            this._name = 'jane Doe'
+            this._age = 30
+            this._gender = 'female'
+            this._level = 'intermidiate'
+        }
+    }
+
+    public introduce() {
+        console.log("Hi, I'm " + this._name + ", a " + this._age + " year old " + this._gender + ' ' + this._level + " mentor.")
+    }
+
+    public getGoal() {
+        console.log("Educate brilliant junior software developers.")
+    }
 }
