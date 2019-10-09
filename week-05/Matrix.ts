001929626a0ddf3758fe26544674beca927a49d0
abdallahgeha <geha.abdallah@gmail.com>
2019-10-09 17:25:40 +0200

    rotating matrix

--- week-05/Matrix.ts
+++ week-05/Matrix.ts
@@ -1,16 +1,16 @@
 'use strict';
 
-let matrix : number [][]= [[1,2,3],[4,5,6],[7,8,9]];
-/*
-[
-[1,2,3],
-[4,5,6],
-[7,8,9]
-]
-[
-[7,4,1],
-[8,5,2],
-[9,6,3]
-]
-*/
-console.log(matrix)
+//practice for rotating
+let matrixX: number [][] = [[1,2,3],[4,5,6],[7,8,9]];
+function rotate(matrix: number [][]){
+  let rotatedMatrix: number [][] = [];
+    for (let i: number = 0; i < matrix.length; i++){
+      let line: number [] = [];
+        for (let j: number = 0; j < matrix[i].length; j++){ //matrix[i] because it is the first line what we created
+          let temp: number = matrix[matrix.length-j][i];
+          line.push(temp);
+        }
+      rotatedMatrix.push(line);
+    }
+    return rotatedMatrix;
+} console.log(rotate(matrixX));
